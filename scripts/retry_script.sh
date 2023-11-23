e9ea06391f42f5ba4f61c0877f78d41d5356b234
Iliya Savov <isavov@users.noreply.github.com>
2023-11-23 17:40:13 +0200

    x
    Signed-off-by: Iliya Savov <isavov@users.noreply.github.com>

--- scripts/retry_script.sh
+++ scripts/retry_script.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 set -v
 set +e
-kill -9 $(cat /tmp/relay.pid)
-kill -9 $(cat /tmp/relay-ws.pid)
+kill -- -$(ps -o pgid= $(pgrep -f "npm run start"))
 hedera restart -d
 sleep 5
 docker stop json-rpc-relay json-rpc-relay-ws
 npm run start &
-echo $! >/tmp/relay.pid
 npm run start:ws &
-echo $! >/tmp/relay-ws.pid
