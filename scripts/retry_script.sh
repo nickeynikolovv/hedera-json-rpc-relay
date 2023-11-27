69236486585bffe2bd72c9c1d40b0dbb0dfd0fcf
Iliya Savov <isavov@users.noreply.github.com>
2023-11-27 12:44:51 +0200

    test
    Signed-off-by: Iliya Savov <isavov@users.noreply.github.com>

--- scripts/retry_script.sh
+++ scripts/retry_script.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 set -v
 set +e
+hedera stop
+sleep 5
+docker ps -a
+docker volume ls
+docker network ls
 docker kill $(docker ps -q)
 docker rm -v $(docker ps -qa)
-hedera stop
+docker system prune --volumes -f
 sleep 5
 hedera restart -d
 docker stop json-rpc-relay json-rpc-relay-ws
